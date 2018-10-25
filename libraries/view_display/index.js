d4eab89e4fcd212e56a0af0f84e019fabb846c8a
Błażej Owczarczyk <blazeyy@gmail.com>
2018-10-25 11:11:05 +0200

    Removed the dependency on jQuery.

--- libraries/view_display/index.js
+++ libraries/view_display/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Display from './Display';
 
-(function ($, Drupal) {
+(function (Drupal) {
 
   Drupal.behaviors.moderation_state_columns_view_display = {
-    attach: function (context, settings) {
-      $('.moderation-state-columns--component-container .moderation-state-columns--json-content', context).each(function () {
-        const { entities, states } = JSON.parse($(this).text());
-        ReactDOM.render(<Display entities={entities} states={states} />, $(this).parent().get(0));
-        $(this).remove();
+    attach: function (context) {
+      const displays = context.querySelectorAll('.moderation-state-columns--component-container .moderation-state-columns--json-content');
+      displays.forEach(element => {
+        const { entities, states } = JSON.parse(element.innerText);
+        ReactDOM.render(<Display entities={entities} states={states} />, element.parentNode);
+        element.remove();
       });
     }
   };
 
-})(jQuery, Drupal);
+})(Drupal);
